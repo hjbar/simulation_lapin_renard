6f29aaf6d7900614f6f157e006e0b01f4a6d482a
hjbar <hjbar@protonmail.com>
2022-05-03 23:49:39 +0200

    Bug avec la fonction deplaceAnimal

--- src/main.cpp
+++ src/main.cpp
@@ -9,4 +9,12 @@ int main()
 	Jeu j{};
 
 	j.affiche();
+
+	Ensemble e = j.getPop().getIds();
+	for(int i = 0; i < e.cardinal(); i++)
+	{
+		j.deplaceAnimal(j.getPop().get(e.getCase(i)));
+	}
+
+	j.affiche();
 }
